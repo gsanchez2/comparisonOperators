2ea49acff8d055f77e0db47700b01d638a9b3f06
gsanchez2 <58314861+gsanchez2@users.noreply.github.com>
2019-12-02 20:50:14 -0600

    Initial commit

---
+++ practiceComparisonOperators.py
@@ -0,0 +1,61 @@
+'''
+Created on Dec 2, 2019
+
+@author: ITAUser
+'''
+
+4 == 4
+True
+
+var = 4 == 4
+print (var)
+
+a = 2 == 4
+False 
+
+print (a)
+
+b = 2 < 3
+True 
+
+print (b) 
+
+c = 4 < 3
+False
+
+print (c)
+
+d = 4 > 3
+True
+
+print (d)
+
+e = 2 > 3
+False
+
+print (e)
+
+a = 2 <= 3
+True
+
+print (a)
+
+b = 2 >= 3
+False 
+
+print (b)
+
+print (e == b)
+
+r = e == b
+print (r)
+
+string1 = "micheal"
+string2 = "Micheal"
+
+print ("micheal" == "Micheal")
+
+a = [1, 2, 3] == [1, 2, 3]
+print (a)
+
+
